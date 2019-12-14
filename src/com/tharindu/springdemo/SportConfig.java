f9773ec7df03629ffbb571912d0b173cc1bb197f
tharindu <tharindumunaweera94@gmail.com>
2019-12-14 20:34:25 +0530

    java configuration n spring bean manually

--- src/com/tharindu/springdemo/SportConfig.java
+++ src/com/tharindu/springdemo/SportConfig.java
@@ -1,10 +1,23 @@
 package com.tharindu.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.tharindu.springdemo")
+//@ComponentScan("com.tharindu.springdemo")
 public class SportConfig {
 
+	// define the bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our swim coach and inject dependencies
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
+	
 }
---
+++ src/com/tharindu/springdemo/SwimCoach.java
@@ -0,0 +1,23 @@
+package com.tharindu.springdemo;
+
+public class SwimCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
+	public String getDailtyWorkout() {
+		
+		return "Swim 1000 meters as a warm up";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
+	}
+
+}
