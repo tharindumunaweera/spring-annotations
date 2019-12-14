73fc4a74980ab81e5d9921973b9cad02f78c7b38
tharindu <tharindumunaweera94@gmail.com>
2019-12-14 13:27:43 +0530

    spring configuration with java code

---
+++ src/com/tharindu/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.tharindu.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.tharindu.springdemo")
+public class SportConfig {
+
+}
